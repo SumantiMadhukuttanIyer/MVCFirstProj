8ab4f97da6ed829afc2ed7e810cfab764c9ba701
SHRADDHA <SHRADDHA@LAPTOP-8OGRSAUK>
2022-05-24 18:38:22 +0530

    Add project files.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,19 @@
+﻿using MVCFirstProj.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace MVCFirstProj.Controllers;
+
+public class FirstController : Controller
+{
+    // Index Action
+
+    // Hello Action
+
+    public IActionResult Info()
+    {
+        Person person = new Person();
+        person.Name = "Sherin";
+        person.Age = 21;
+        person.Location = "Ukarine";
+        return View(person);
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MVCFirstProj.Models;
+
+public class Person
+{
+    public string Name { get; set; } = String.Empty;
+    public int Age { get; set; }
+    public string Location { get; set; } = String.Empty;
+}
